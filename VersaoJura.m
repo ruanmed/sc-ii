f32e831946ecfa7959aa3c1c65c010b58d33201b
Ricardo Medeiros <j_r_style@hotmail.com>
2019-08-02 19:30:10 -0300

    New project using professor specifications
    Added new pole to the controler to avoid derivative instability

--- VersaoJura.m
+++ VersaoJura.m
@@ -1,24 +1,27 @@
 np =[1]
 dp = conv([1 6 0],[1 10])
 Gp = tf(np,dp)
-figure(8)
+figure(1)
 rlocus(Gp)
-nc = [1 8.269]
-dc = [1]
+nc = [1 6.3]
+dc = [1 35.57]
 Gc = tf(nc,dc)
-k = 67.28
+%k = 2459.85 %calculo da condição de módulo
+k = 1100
 G=k*Gc*Gp
 gmf = G/(1+G)
-figure(7)
-rlocus(Gc*Gp)
-figure(1)
-step(gmf)
 figure(2)
-bode(G)
-T=0.15/6.67
-Gz = c2d(G,T,'tustin')
-GmfZ = Gz/(1+Gz)
+rlocus(Gc*Gp)
 figure(3)
+step(gmf)
+figure(4)
+bode(G)
+T=0.15/7.71
+Gcz =c2d(k*Gc,T,'tustin')
+Gpz =c2d(Gp,T,'tustin')
+Gz = c2d(G,T,'tustin');
+GmfZ = Gz/(1+Gz)
+figure(5)
 step(GmfZ)
 figure(4)
 rlocus(Gz)
--- comunicacao.ino
+++ comunicacao.ino
@@ -1,8 +1,8 @@
-double Er[2] = {0.0, 0.0}; // variável para o dado recebido
-double Ec = 0.0;
+float Er[2] = {0.0, 0.0}; // variável para o dado recebido
+float Ec = 0.0;
 void setup() {
   Serial.begin(9600); // abre a porta serial, configura a taxa de transferência para 9600 bps
-  Serial.println(Ec, 8);//Enviando esforço do controlador para a planta
+  Serial.println(Ec);//Enviando esforço do controlador para a planta
 }
 
 void loop() {
@@ -10,11 +10,12 @@ void loop() {
     // Enquanto não tiver resposta disponível não faz nada
   }
   Er[1] = Er[0];
-  Er[0] = Serial.parseFloat(); // lê do buffer o dado recebido e já transforma em float    
+  Er[0] = (1 -Serial.parseFloat()); // lê do buffer o dado recebido e já transforma em float    
   
   // Calcula o valor controlado, o esforço do controlador
-  Ec = Er[0]*58.17-45.57*Er[1]-Ec;  
+  Ec = 867.3*Er[0]-767.1*Er[1]+0.4859*Ec; 
+  //Ec = abs(Ec);
 
   // Retorna o valor para a planta
-  Serial.println(Ec, 8);
+  Serial.println(Ec);
 }
