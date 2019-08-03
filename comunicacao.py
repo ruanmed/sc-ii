56fcc623731a6d3567e8cd691b3c52deb27a82a9
Ricardo Medeiros <j_r_style@hotmail.com>
2019-08-02 21:28:07 -0300

    Error: controller doesn't stop
    it keep it effort constant

--- comunicacao.py
+++ comunicacao.py
@@ -6,7 +6,9 @@
 import serial 
 import struct
 
-arduinoSerial = serial.Serial('/dev/ttyUSB0', 9600)
+#arduinoSerial = serial.Serial('/dev/ttyUSB0', 9600)
+arduinoSerial = serial.Serial('COM5', 9600)
+
 Y = [0.0, 0.0, 0.0, 0.0]
 Ec = [0.0, 0.0, 0.0, 0.0]
 #ser.write(b'5') #Prefixo b necessario se estiver utilizando Python 3.X
@@ -26,9 +28,11 @@ while True:
     Y[3] = Y[2]
     Y[2] = Y[1]
     Y[1] = Y[0]
-    Y[0]= Ec[0] * 5.386e-06  + Ec[1]*1.616e-05 + Ec[2]* 1.616e-05 + Ec[3]*5.386e-06 + Y[1]* 2.469 - Y[2]*2.006 + Y[3]*0.5364
+    #Y[0]= Ec[0] * 5.386e-06  + Ec[1]*1.616e-05 + Ec[2]* 1.616e-05 + Ec[3]*5.386e-06 + Y[1]* 2.469 - Y[2]*2.006 + Y[3]*0.5364
+    Y[0] = 7.926e-07*Ec[0] + 2.378e-06*Ec[1] + 2.378e-06*Ec[2] + 7.926e-07*Ec[3] + 2.712*Y[1] -2.444*Y[2] +0.732*Y[3]
+# Retorna o valor para o controlador
+#data = arduinoSerial.write(struct.pack('f', Y[0]))
+    arduinoSerial.write(b'str(Y[0])')
+    print(Ec,Y)
+	
     
-
-    # Retorna o valor para o controlador
-    data = arduinoSerial.write(struct.pack('d', Y[0]))
-    print(data)
