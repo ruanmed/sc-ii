619d17b2456a162f36a8c727c11a2dff7fd50307
Ricardo Medeiros <j_r_style@hotmail.com>
2019-08-03 01:28:49 -0300

    Added graphics
     step response and controller effort graphics added

--- comunicacao.py
+++ comunicacao.py
@@ -5,16 +5,21 @@
 #Import Libraries:
 import serial 
 import struct
+import matplotlib.pyplot as plt
 
-arduinoSerial = serial.Serial('/dev/ttyACM0', 9600)
-# arduinoSerial = serial.Serial('COM5', 9600)
+#arduinoSerial = serial.Serial('/dev/ttyACM0', 9600)
+arduinoSerial = serial.Serial('COM5', 9600)
 
+Yseries = []
+ECseries = []
+Tseries = []
+k = 0
+samplingTime = 0.019455
 Y = [0.0, 0.0, 0.0, 0.0]
 Ec = [0.0, 0.0, 0.0, 0.0]
 #ser.write(b'5') #Prefixo b necessario se estiver utilizando Python 3.X
 
-while True:
-
+while k*samplingTime<2:
     while arduinoSerial.in_waiting == 0:
         pass # Enquanto não tiver resposta disponível não faz nada
 
@@ -31,10 +36,26 @@ while True:
     #Y[0]= Ec[0] * 5.386e-06  + Ec[1]*1.616e-05 + Ec[2]* 1.616e-05 + Ec[3]*5.386e-06 + Y[1]* 2.469 - Y[2]*2.006 + Y[3]*0.5364
     # Y[0] = 7.926e-07*Ec[0] + 2.378e-06*Ec[1] + 2.378e-06*Ec[2] + 7.926e-07*Ec[3] + 2.712*Y[1] -2.444*Y[2] +0.732*Y[3]
     Y[0] = 0.0000007926*Ec[0] + 0.000002378*Ec[1] + 0.000002378*Ec[2] + 0.0000007926*Ec[3] + 2.712*Y[1] -2.444*Y[2] +0.732*Y[3]
-    
+    Yseries.append(Y[0])
+    ECseries.append(Ec[0])
+    Tseries.append(k*samplingTime)
+    k = k+1
     # Retorna o valor para o controlador
     #data = arduinoSerial.write(struct.pack('f', Y[0]))
     arduinoSerial.write(str(str(float(Y[0]))+'\n').encode())
-    print(Ec,Y) # para fins de debug
-	
-    
+    print(Ec[0],Y[0]) # para fins de debug
+
+plt.figure(1)
+plt.plot(Tseries,Yseries)
+plt.title('step response')
+plt.ylabel('Intensity')
+plt.xlabel('time - s')
+
+
+plt.figure(2)
+plt.plot(Tseries,ECseries)
+plt.title('controller effort')
+plt.ylabel('Intensity')
+plt.xlabel('time - s')
+
+plt.show()
