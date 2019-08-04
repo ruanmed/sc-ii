eb11dc783ba2427a57dd31a4839a3204db545f18
Ricardo Medeiros <j_r_style@hotmail.com>
2019-08-04 15:01:52 -0300

    Remoção do ganho da função de transferência
    Para ajuste utilizando o microcontrolador de forma facilitada

--- MATLAB/VersaoJura.m
+++ MATLAB/VersaoJura.m
@@ -4,10 +4,10 @@ Gp = tf(np,dp)
 figure(1)
 rlocus(Gp)
 nc = [1 6.3]
-dc = [1 35.57]
+dc = [1 35.57]%condiÃ§Ã£o de Ã¢ngulo
 Gc = tf(nc,dc)
-%k = 2459.85 %calculo da condição de módulo
-k = 1100
+%k = 2459.85 %calculo da condiï¿½ï¿½o de MÃ³dulo
+k = 1000
 G=k*Gc*Gp
 gmf = G/(1+G)
 figure(2)
@@ -17,11 +17,11 @@ step(gmf)
 figure(4)
 bode(G)
 T=0.15/7.71
-Gcz =c2d(k*Gc,T,'tustin')
+Gcz =c2d(Gc,T,'tustin')
 Gpz =c2d(Gp,T,'tustin')
 Gz = c2d(G,T,'tustin');
 GmfZ = Gz/(1+Gz)
 figure(5)
 step(GmfZ)
-figure(4)
+figure(6)
 rlocus(Gz)
